4a1895c6489a5acc7a4a89cf2d29b76b75fe8020
Afri <afrifranck@gmail.com>
2024-06-28 23:53:53 +0000

    Save(rien d'important)

--- models/car_management_travel.py
+++ models/car_management_travel.py
@@ -13,18 +13,14 @@ class Travel(models.Model):
     destination = fields.Char(string="Destination")
     date_departure = fields.Datetime(string="Date de départ", default=fields.Datetime.now())
     date_arrival = fields.Datetime(string="Date d'arrivée", default=fields.Datetime.now())
-    travel_time = fields.Integer(string="Durée du voyage", inverse="_inverse_travel_time", default=1)
+    travel_time = fields.Datetime(string="Durée du voyage")
     numbers_of_places = fields.Integer(
         string="Nombres de places",
         readonly=True,
         compute="_compute_numbers_of_places"
     )
 
-    def _inverse_travel_time(self):
-        for record in self:
-            record.travel_time = record.travel_time * 2
-
-    @api.onchange("car_id")
+    @api.depends("car_id")
     def _compute_numbers_of_places(self):
         for record in self:
             record.numbers_of_places = record.car_id.numbers_of_places

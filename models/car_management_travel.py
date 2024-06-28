b895231d3563a2304e9e4932c24f0981e5dbf205
Afri <afrifranck@gmail.com>
2024-06-28 22:57:03 +0000

    Le champ numbers of places du model Travel Ãest désormais un champs caculé

--- models/car_management_travel.py
+++ models/car_management_travel.py
@@ -14,14 +14,18 @@ class Travel(models.Model):
     date_departure = fields.Datetime(string="Date de départ", default=fields.Datetime.now())
     date_arrival = fields.Datetime(string="Date d'arrivée", default=fields.Datetime.now())
     travel_time = fields.Integer(string="Durée du voyage", inverse="_inverse_travel_time", default=1)
-    numbers_of_places = fields.Integer(string="Nombre de place")
+    numbers_of_places = fields.Integer(
+        string="Nombres de places",
+        readonly=True,
+        compute="_compute_numbers_of_places"
+    )
 
     def _inverse_travel_time(self):
         for record in self:
             record.travel_time = record.travel_time * 2
 
     @api.onchange("car_id")
-    def _onchange_car_id(self):
+    def _compute_numbers_of_places(self):
         for record in self:
             record.numbers_of_places = record.car_id.numbers_of_places
 

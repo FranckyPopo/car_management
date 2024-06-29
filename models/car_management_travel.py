2e7dcecae5e4f11758e8c74e59116f9030b36638
Afri <afrifranck@gmail.com>
2024-06-29 18:20:36 +0000

    Des Erreur son saffiché lorsque lÃdeles date de dsépart et d'arrivéÃe du modèle Car son incohérente

--- models/car_management_travel.py
+++ models/car_management_travel.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 from pprint import pprint
 
 
@@ -36,15 +38,14 @@ class Travel(models.Model):
             if calcule_travel_time.days :
                 travel_time = f"{calcule_travel_time.days} jour(s)"
 
+            # Calcule des heure(s) et minute(s)
             if calcule_travel_time.seconds:
                 total_minutes = calcule_travel_time.seconds // 60
 
-                # Calcule des heure(s) et minute(s)
                 if total_minutes:
                     total_hours = total_minutes // 60
 
                     if total_hours > 0:
-                        print("Oui 1")
                         travel_time = travel_time + f" {total_hours} heure(s)"
 
                     # Heures et minutes restante
@@ -59,9 +60,27 @@ class Travel(models.Model):
 
             record.travel_time = f"{travel_time}"
 
+    @api.constrains("date_arrival")
+    def _check_date_arrival(self):
+        for record in self:
+            format_date_arrival_fr = record.date_arrival.strftime("%d/%m/%Y %H:%M")
+            format_date_departure_fr = record.date_departure.strftime("%d/%m/%Y %H:%M")
+
+            if record.date_arrival == record.date_departure:
+                raise ValidationError(
+                    f"""La date d'arrivé ne doit pas être égale à la date de départ
+                    \n Date d'arrivée: {format_date_arrival_fr} \n Date de dépar:t {format_date_departure_fr}
+                    """
+                )
+
+            if record.date_departure > record.date_arrival:
+                raise ValidationError(
+                    f""" La date d'arrivé ne doit pas être inférieux à la date départ. 
+                    \n Date d'arrivé: {format_date_arrival_fr} \n Date de départ: {format_date_departure_fr}
+                    """
+                )
+
 
     def _compute_display_name(self):
         for order in self:
             order.display_name = f"{order.car_id.car_model}"
-
-
